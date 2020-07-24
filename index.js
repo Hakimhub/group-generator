b2e72927b7f68a9e5259f85f136101b3b2cfd296
Hakimhub <67559215+Hakimhub@users.noreply.github.com>
2020-07-24 10:44:25 +0200

    creat collection

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-console.log("Hello");
-console.log('Weeeshhh')
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url,{useNewUrlParser: true, useUnifiedTopology: true}, function(err, db){
+    if (err) throw err;
+   let dbo = db.db("mydb");
+   let myobj = { name: "Hakim"}
+   dbo.collection("Students").insertOne(myobj, function(err, res){
+       if (err) throw err;
+       console.log("Collection Students created!");
+
+   })
+   dbo.createCollection("Groups", function(err, res){
+       if (err) throw err;
+       console.log("Collection Groups created!");
+       db.close();
+   });
+    
+});
+

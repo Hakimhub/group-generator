ebaa1e0c76cc07e1e6fd6b2878bf76cf9721767c
Hakimhub <67559215+Hakimhub@users.noreply.github.com>
2020-07-24 17:07:19 +0200

    delete collection

--- index.js
+++ index.js
@@ -11,13 +11,17 @@ MongoClient.connect(url,{useNewUrlParser: true, useUnifiedTopology: true}, funct
     let myTab;                                          //--- Create a variable myTab with no value, the value will be defined later
     let mydb = db.db("mydb");                            //--- Define the name of the database we will use
 
-app.get("/students",async function (req,res){
-    let getDB = await mydb.collection("Students").find({}).toArray()
-    res.send(getDB)
+    //GET
+    app.get("/students",async function (req,res){          //---WE create a get request that use the route "/student". we get the data of mongodb student collection.  
+    let getDB = await mydb.collection("Students").find({}).toArray() //---In the getDB variable we store the elements of the collection student in an Array. we use await to wait untill all the elements are stored before we move on.
+    res.send(getDB)                                       //--- we send our elements that are in getDB to the '/students' route. we can see them in postman with get request.
+
+    //DELETE
+
 
 })
 
-    app.post("/students", function(req, res) {          //--- We create a "POST" command that will use the "localhost:3000/students" route and when we send a POST request in Postman, it run the function
+    app.post("/students", function(req, res) {          //--- We create a "POST" request that will use the "localhost:3000/students" route and when we send a POST request in Postman, it run the function
         myTab = req.body.name                           //--- We define the value of myTab. req.body.name ==> The values we put in Postman ["hakim", "arthur", "lou", ...]
         console.log(myTab)                              
         myTab.forEach(element => mydb.collection("Students").insertOne({name: element}, function(err, res){ //--- We go into myTab to check every element in myTab and we send to the collection "Students" the elements in this form: {name: element}. The elements are now in our database in the Students collection

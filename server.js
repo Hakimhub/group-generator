c63993560182a11ec3580531da765a4e251879e9
Arthur <morinarthurpro@gmail.com>
2020-07-29 08:43:11 +0200

    group post en partie

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express()
 const fetch = require('node-fetch');
 const path = require('path');
+const { type } = require("os");
 const url = "http://localhost:3000"
 
 app.use(express.urlencoded({extended: true}))
@@ -25,9 +26,7 @@ app.post('/students', async function (req, res) {
      fetch(`${url}/students`, {method: 'POST', headers: {
         "Content-type": "application/json"
         //"Content-type": "application/json; raw"
-    }, body: JSON.stringify({name: name})}).then(function () {
-        
-    })
+    }, body: JSON.stringify({name: name})})
     let studentList = await fetch((`${url}/students`));
     let jsonStudent = await studentList.json()
     res.render(`students`, {jsonStudent: jsonStudent})
@@ -42,6 +41,32 @@ app.get('/groups', async function (req, res) {
 
     res.render('groups', {jsonGroup: jsonGroup} )
 })
+
+app.post('/groups', async function (req, res) {
+    const groupe =  req.body.groupe;
+    const num = parseInt(req.body.nbr, 10)
+    const list = await fetch(`${url}/students`);
+    const jsonList = await list.json()
+    let newGroup = {
+        groupe: groupe,
+        name: []
+    }
+
+    for (let i = 0; i < num; i ++) {
+        let numRandom = Math.floor(Math.random() * (jsonList.length + 1))
+        let getObj = jsonList[numRandom]
+        newGroup.name.push(getObj.name)
+    }
+    fetch(`${url}/groups`, {method: 'POST', headers: {
+        "Content-type": "application/json"
+        //"Content-type": "application/json; raw"
+    }, body: JSON.stringify(newGroup)})
+
+    let groupList = await fetch(`${url}/groups`)
+    let jsonGroup = await groupList.json()
+    res.render('groups', {jsonGroup: jsonGroup} )
+})
+
 
 
 app.listen(8080, function () {                          //--- Connection to the server on the port 8080
